872c70eacfacfa03dfb6f1d2a3f4662fe7feb936
k9rosie <k9rosie@gmail.com>
2016-05-05 14:40:06 -0500

    second commit

--- areial-imaging/areial-imaging.ino
+++ areial-imaging/areial-imaging.ino
@@ -1,3 +1,4 @@
+#include <NewTone.h>
 #include <Servo.h>
 #include <NewPing.h>
 #include <SFE_BMP180.h>
@@ -10,24 +11,51 @@
 #define SERVO 6
 
 // max distance in centimeters
-int maxDistance = 200;
+int maxDistance = 30;
+
+NewPing sonar(SR04_TRIGGER, SR04_ECHO, maxDistance);
+SFE_BMP180 altometer;
+
+double baselinePressure;
+double initialAltitude = 0.0;
 
 void setup() {
+	Serial.begin(9600);
 	pinMode(PIEZO, OUTPUT);
 	pinMode(LED, OUTPUT);
+	
+	if (altometer.begin()) {
+    	Serial.println("BMP180 init success");
+    	baselinePressure = getPressure();
+    	initialAltitude = altometer.altitude(getPressure(), baselinePressure);
+  	} else {
+    	Serial.println("BMP180 init fail (disconnected?)\n\n");
+   	}
+
 }
 
 void loop() {
-	ledTest();
+	int distance = sonar.ping_cm();
+	double delta = altometer.altitude(getPressure(), baselinePressure);
+	Serial.println(delta*3.28084);
+	if (delta > 2.0) {
+		digitalWrite(LED, HIGH);
+	} else {
+		digitalWrite(LED, LOW);
+	}
 }
 
-void piezoTest() {
+double getPressure() {
+	char status = altometer.startTemperature();
+	double temperature;
+	double pressure;
 
-}
+	delay(status);
 
-void ledTest() {
-	delay(100);
-	digitalWrite(LED_PIN, HIGH);
-	delay(100);
-	digitalWrite(LED_PIN, LOW);
+	altometer.getTemperature(temperature);
+	status = altometer.startPressure(3);
+	delay(status);
+	altometer.getPressure(pressure, temperature);
+
+	return(pressure);
 }
