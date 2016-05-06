fc66e0b63b4e61a96f5fce86d0ff077ad7dfb724
k9rosie <k9rosie@gmail.com>
2016-05-06 15:03:32 -0500

    4

--- areial-imaging/areial-imaging.ino
+++ areial-imaging/areial-imaging.ino
@@ -1,9 +1,9 @@
-#include <NewTone.h>
+//#include <NewTone.h>
 #include <Servo.h>
 #include <NewPing.h>
 #include <SFE_BMP180.h>
 #include <Wire.h>
-#include <math.h>
+//#include <math.h>
 
 #define PIEZO 2
 #define LED 3
@@ -12,7 +12,7 @@
 #define SERVO 6
 
 #define MAX_DISTANCE 30 // in centimeters
-#define MAX_HEIGHT 3.00 // in feet
+#define MAX_HEIGHT 40.00 // in feet
 #define OVERSAMPLING 3
 #define SAMPLING_RATE 10
 
@@ -23,6 +23,7 @@ SFE_BMP180 barometer;
 Servo servo;
 
 bool heightReached = false;
+bool grounded = false;
 
 void setup() {
 	Serial.begin(9600);
@@ -37,14 +38,11 @@ void loop() {
 	int distance = sonar.ping_cm();
 	double pressure = getPressure();
 	double altitude = 0.0;
-
 	// take a large number of readings and average them to improve accuracy
 	for (int i = 0; i < SAMPLING_RATE; i++) {
 		altitude += barometer.altitude(pressure, baselinePressure)*3.28084;
 	}
 	altitude /= SAMPLING_RATE;
-
-	Serial.println(altitude);
 
 	if (altitude >= MAX_HEIGHT) {
 		heightReached = true;
@@ -52,9 +50,18 @@ void loop() {
 
 	if (heightReached) {
 		digitalWrite(LED, HIGH);
-		sweep();
+		if (!grounded) {
+			sweep();
+		}
+		if (distance != 0 && distance <= 10) {
+			tone(PIEZO, 400);
+			grounded = true;
+		} else {
+			noTone(PIEZO);
+		}
+		
 	}
-
+	
 }
 
 double getPressure() {
@@ -74,13 +81,13 @@ double getPressure() {
 
 void sweep() {
 	int pos = 0;
-	for (; pos <= 360; pos++) {
+	for (; pos <= 135; pos++) {
 		servo.write(pos);
-		delay(1);
+		delay(5);
 	}
 
 	for (; pos >= 0; pos--) {
 		servo.write(pos);
-		delay(1);
+		delay(5);
 	}
 }
